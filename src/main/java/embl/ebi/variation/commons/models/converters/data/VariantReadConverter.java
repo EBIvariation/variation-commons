ebbc1d5685fb2f96e190f75d70fa1fd891a83145
Tom Smith <tsmith@ebi.ac.uk>
2016-09-21 14:35:56 +0100

    Convert by wrapping DBObjectToVariantConverter

--- src/main/java/embl/ebi/variation/commons/models/converters/data/VariantReadConverter.java
+++ src/main/java/embl/ebi/variation/commons/models/converters/data/VariantReadConverter.java
@@ -2,6 +2,7 @@ package embl.ebi.variation.commons.models.converters.data;
 
 import com.mongodb.DBObject;
 import org.opencb.biodata.models.variant.Variant;
+import org.opencb.opencga.storage.mongodb.variant.DBObjectToVariantConverter;
 import org.springframework.core.convert.converter.Converter;
 
 /**
@@ -11,7 +12,8 @@ public class VariantReadConverter implements Converter<DBObject, Variant> {
 
     @Override
     public Variant convert(DBObject dbObject) {
-        return null;
+        DBObjectToVariantConverter dbObjectToVariantConverter = new DBObjectToVariantConverter();
+        return dbObjectToVariantConverter.convertToDataModelType(dbObject);
     }
 
 }
