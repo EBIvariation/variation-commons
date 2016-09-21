cae5f7380bc112ac56f91a6e90d5e849a4a78f19
Tom Smith <tsmith@ebi.ac.uk>
2016-09-21 15:59:33 +0100

    Used constructor of DBObjectToVariantConverter that  allows file information extraction

--- src/main/java/embl/ebi/variation/commons/models/converters/data/VariantReadConverter.java
+++ src/main/java/embl/ebi/variation/commons/models/converters/data/VariantReadConverter.java
@@ -2,7 +2,11 @@ package embl.ebi.variation.commons.models.converters.data;
 
 import com.mongodb.DBObject;
 import org.opencb.biodata.models.variant.Variant;
+import org.opencb.opencga.storage.core.variant.VariantStorageManager;
+import org.opencb.opencga.storage.mongodb.variant.DBObjectToSamplesConverter;
 import org.opencb.opencga.storage.mongodb.variant.DBObjectToVariantConverter;
+import org.opencb.opencga.storage.mongodb.variant.DBObjectToVariantSourceEntryConverter;
+import org.opencb.opencga.storage.mongodb.variant.DBObjectToVariantStatsConverter;
 import org.springframework.core.convert.converter.Converter;
 
 /**
@@ -12,8 +16,12 @@ public class VariantReadConverter implements Converter<DBObject, Variant> {
 
     @Override
     public Variant convert(DBObject dbObject) {
-        DBObjectToVariantConverter dbObjectToVariantConverter = new DBObjectToVariantConverter();
-        return dbObjectToVariantConverter.convertToDataModelType(dbObject);
+        DBObjectToVariantConverter converter =
+                new DBObjectToVariantConverter(
+                        new DBObjectToVariantSourceEntryConverter(VariantStorageManager.IncludeSrc.FULL),
+                        new DBObjectToVariantStatsConverter()
+                );
+        return converter.convertToDataModelType(dbObject);
     }
 
 }
