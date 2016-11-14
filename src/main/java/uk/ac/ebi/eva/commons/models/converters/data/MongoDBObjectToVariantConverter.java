c9f5c0df3d5674ad72f4ba736b3a153569e8ce7b
Tom Smith <tsmith@ebi.ac.uk>
2016-11-14 15:57:09 +0000

    Different DBObjectToVariantSourceEntryConverter initialisation due to opencga version (hotfix/0.5). WORKS

--- src/main/java/uk/ac/ebi/eva/commons/models/converters/data/MongoDBObjectToVariantConverter.java
+++ src/main/java/uk/ac/ebi/eva/commons/models/converters/data/MongoDBObjectToVariantConverter.java
@@ -6,6 +6,8 @@ import org.opencb.opencga.storage.core.variant.VariantStorageManager;
 import org.opencb.opencga.storage.mongodb.variant.DBObjectToVariantConverter;
 import org.opencb.opencga.storage.mongodb.variant.DBObjectToVariantSourceEntryConverter;
 import org.opencb.opencga.storage.mongodb.variant.DBObjectToVariantStatsConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.convert.converter.Converter;
 
 /**
@@ -13,11 +15,16 @@ import org.springframework.core.convert.converter.Converter;
  */
 public class MongoDBObjectToVariantConverter implements Converter<DBObject, Variant> {
 
+    protected static Logger logger = LoggerFactory.getLogger(MongoDBObjectToVariantConverter.class);
+
     @Override
     public Variant convert(DBObject dbObject) {
+
+        logger.warn("INSIDE MongoDBObjectToVariantConverter.convert");
+
         DBObjectToVariantConverter converter =
                 new DBObjectToVariantConverter(
-                        new DBObjectToVariantSourceEntryConverter(true),
+                        new DBObjectToVariantSourceEntryConverter(VariantStorageManager.IncludeSrc.NO),
                         new DBObjectToVariantStatsConverter()
                 );
         return converter.convertToDataModelType(dbObject);
