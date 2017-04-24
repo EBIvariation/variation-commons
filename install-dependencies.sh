47f025e30d29b0448a197580c2e78a7eaf1a446b
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-04-24 09:37:31 +0100

    The dependencies are in a central repository, and they don't need to be installed in the travis build (#28)

--- install-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-git clone -b hotfix/0.4 https://github.com/ebivariation/biodata.git
-git clone -b hotfix/0.5 https://github.com/ebivariation/opencga.git
-
-cd biodata && mvn install
-cd ..
-cd opencga && mvn install -DskipTests
-
