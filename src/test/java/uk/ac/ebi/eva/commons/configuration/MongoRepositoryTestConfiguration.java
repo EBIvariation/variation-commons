1458a2710d4ea6992a9dfe2b543936bb36529b26
jorizci <jorizci@gmail.com>
2017-06-01 10:51:34 +0100

    Moved mongodb persistence classes from eva-lib in eva-ws to variation commons

---
+++ src/test/java/uk/ac/ebi/eva/commons/configuration/MongoRepositoryTestConfiguration.java
@@ -0,0 +1,58 @@
+/*
+ * European Variation Archive (EVA) - Open-access database of all types of genetic
+ * variation data from all species
+ *
+ * Copyright 2017 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.eva.commons.configuration;
+
+import com.github.fakemongo.Fongo;
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "uk.ac.ebi.eva.commons.mongodb.repositories")
+@Import({MongoFactoriesConfiguration.class})
+@PropertySource({"classpath:eva.properties"})
+public class MongoRepositoryTestConfiguration {
+
+    @Bean
+    public MongoClient mongoClient() {
+        return new Fongo("defaultInstance").getMongo();
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDbFactory mongoDbFactory,
+            MappingMongoConverter mappingMongoConverter) throws Exception {
+        return new MongoTemplate(mongoDbFactory, mappingMongoConverter);
+    }
+
+    @Bean
+    public MongoDbFactory mongoDbFactory(MongoClient mongoClient) throws Exception {
+        return new SimpleMongoDbFactory(mongoClient, this.getDatabaseName());
+    }
+
+    private String getDatabaseName() {
+        return "test-db";
+    }
+}
