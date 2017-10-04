91574981b78dc8a4fa81f18db8e307a693b070f8
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-10-04 09:44:37 +0100

    Fixed package in VariantVcfFactory classes

--- variation-commons-core/src/main/java/uk/ac/ebi/eva/commons/core/models/factories/VariantVcfFactory.java
+++ variation-commons-core/src/main/java/uk/ac/ebi/eva/commons/core/models/factories/VariantVcfFactory.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.ac.ebi.eva.pipeline.io.mappers;
+package uk.ac.ebi.eva.commons.core.models.factories;
 
 import org.apache.commons.lang3.StringUtils;
 import org.opencb.biodata.models.feature.Genotype;
