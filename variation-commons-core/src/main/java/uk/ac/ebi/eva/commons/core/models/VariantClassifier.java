9af66879a69e11ec314227303d27de61fca29269
sundarvenkata <sundarvenkata@ebi.ac.uk>
2018-07-09 11:03:05 +0100

    Updated documentation to clarify that trimmed alleles are expected

--- variation-commons-core/src/main/java/uk/ac/ebi/eva/commons/core/models/VariantClassifier.java
+++ variation-commons-core/src/main/java/uk/ac/ebi/eva/commons/core/models/VariantClassifier.java
@@ -26,8 +26,8 @@ public class VariantClassifier {
      * See https://docs.google.com/spreadsheets/d/1YH8qDBDu7C6tqULJNCrGw8uBjdW3ZT5OjTkJzGNOZ4E/edit#gid=1433496764
      * @TODO: might need to be merged eventually into {@link uk.ac.ebi.eva.commons.core.models.AbstractVariant#getType()}
      *
-     * @param reference Reference allele
-     * @param alternate Alternate allele
+     * @param reference Reference allele - expects trimmed allele
+     * @param alternate Alternate allele - expects trimmed allele
      * @param subSNPClass SubSNP class (as defined in dbSNP variant data)
      *                    - See https://www.ncbi.nlm.nih.gov/books/NBK21088/table/ch5.ch5_t3/?report=objectonly
      * @return Type of the variant
