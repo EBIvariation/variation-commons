6122e3375e4118525b6f688f24f093202b05006a
JUNAID N Z <junaidnz97@gmail.com>
2019-06-03 17:54:42 +0530

    Apply suggestions from code review
    Co-Authored-By: Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>

--- variation-commons-mongodb/src/main/java/uk/ac/ebi/eva/commons/mongodb/filter/VariantRepositoryStartFilter.java
+++ variation-commons-mongodb/src/main/java/uk/ac/ebi/eva/commons/mongodb/filter/VariantRepositoryStartFilter.java
@@ -20,7 +20,7 @@ package uk.ac.ebi.eva.commons.mongodb.filter;
 
 import uk.ac.ebi.eva.commons.mongodb.entities.VariantMongo;
 
-public class VariantRepositoryStartFilter extends VariantRepositoryFilter<Long>{
+public class VariantRepositoryStartFilter extends VariantRepositoryFilter<Long> {
 
     private static String FIELD = VariantMongo.START_FIELD;
     public VariantRepositoryStartFilter(Long start, RelationalOperator relationalOperator) {
